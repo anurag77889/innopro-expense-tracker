751f0ea59083b949ab80818a95059dda99d56a7a
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2025-04-03 23:05:56 +0530

    "Changed Dashboard"

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,25 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link"; // Correct import
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 function Header() {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="p-5 flex justify-between items-center shadow-sm">
-      <Image src={"./logo.svg"} alt="logo" width={50} height={50} />
-      <Button>Get Started</Button>
+      <Image src="/logo.svg" alt="logo" width={50} height={50} />
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link href="/sign-in">
+          <Button asChild>
+            <span>Get Started</span>
+          </Button>
+        </Link>
+      )}
     </div>
   );
 }
